3ebf4dea6d4cc8296746da5b515b45400039e505
ElizabethMulokozi <elizaisrael2003@gmail.com>
2025-04-16 13:29:16 +0300

    second commit

--- app/src/main/java/com/example/loginscreen/Dashboard.java
+++ app/src/main/java/com/example/loginscreen/Dashboard.java
@@ -2,11 +2,15 @@ package com.example.loginscreen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,11 +22,14 @@ public class Dashboard extends AppCompatActivity {
 
     private boolean isUserNavigating = false; // Track if user is navigating manually
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
@@ -49,17 +56,6 @@ public class Dashboard extends AppCompatActivity {
 
         });
 
-
-        // Logout button
-        Button btnLogout = findViewById(R.id.btnLogout);
-        btnLogout.setOnClickListener(v -> {
-            isUserNavigating = true;
-            FirebaseAuth.getInstance().signOut();
-            Intent intent = new Intent(Dashboard.this, MainActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
-        });
     }
 
     @Override
@@ -76,5 +72,41 @@ public class Dashboard extends AppCompatActivity {
             FirebaseAuth.getInstance().signOut();
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // Handle menu item clicks
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_contacts) {
+//            startActivity(new Intent(getApplicationContext(), Profile.class));
+//            overridePendingTransition(0, 0);
+            Toast.makeText(this, "Contacts Clicked", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_logout) {
+
+                isUserNavigating = true;
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(Dashboard.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+
+            return true;
+        }
+
+        return false;
+    }
+
 
 }
--- app/src/main/java/com/example/loginscreen/ExploreActivity.java
+++ app/src/main/java/com/example/loginscreen/ExploreActivity.java
@@ -1,12 +1,16 @@
 package com.example.loginscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ExploreActivity extends AppCompatActivity {
 
@@ -15,10 +19,34 @@ public class ExploreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_explore);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_explore);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_explore) {
+                    return true;
+                } else if (id == R.id.nav_home) {
+                    startActivity(new Intent(getApplicationContext(), Dashboard.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                } else if (id == R.id.nav_profile) {
+                    startActivity(new Intent(getApplicationContext(), Profile.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+                }
+
+                return false;
+            }
+
         });
+
+
     }
+
+
 }
